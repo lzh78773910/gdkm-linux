802e6edb5c4b0e3177a906e2e86152983517e97f
EDDIEZZZZ <1300442405@qq.com>
2020-01-02 21:53:21 +0800

    添加用户权限控制

--- src/main/java/com/gdkm/Repository/UserRepository.java
+++ src/main/java/com/gdkm/Repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.gdkm.Repository;
 
 import com.gdkm.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
      User findByuserName(String userName);
      User findByMail(String mail);
+     @Query(value = "update user set status=?1 where user_name=?2",nativeQuery = true)
+     @Modifying
+     public void updateOne(Integer status,String userName);
 }
